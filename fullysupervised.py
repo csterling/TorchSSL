5fbaa869d88f86a87a9ae6743b4cb016d4c38201
csterling <coreytsterling@gmail.com>
2021-12-09 09:30:45 +1300

    Added custom datasets to fullysupervised.

--- fullysupervised.py
+++ fullysupervised.py
@@ -12,6 +12,7 @@ import torch.backends.cudnn as cudnn
 import torch.distributed as dist
 import torch.multiprocessing as mp
 
+from datasets.custom_dataset import CustomDataset2
 from utils import net_builder, get_logger, count_parameters, over_write_args_from_file
 from train_utils import TBLog, get_optimizer, get_cosine_schedule_with_warmup
 from models.fullysupervised.fullysupervised import FullySupervised
@@ -180,13 +181,19 @@ def main_worker(gpu, ngpus_per_node, args):
     if args.rank != 0:
         torch.distributed.barrier()
     # Construct Dataset & DataLoader
-    train_dset = SSL_Dataset(args, alg='fullysupervised', name=args.dataset, train=True,
-                             num_classes=args.num_classes, data_dir=args.data_dir)
-    lb_dset, ulb_dset = train_dset.get_ssl_dset(args.num_labels)
+    if not args.dataset.lower().startswith("custom_"):
+        train_dset = SSL_Dataset(args, alg='fullysupervised', name=args.dataset, train=True,
+                                 num_classes=args.num_classes, data_dir=args.data_dir)
+        lb_dset, ulb_dset = train_dset.get_ssl_dset(args.num_labels)
 
-    _eval_dset = SSL_Dataset(args, alg='fullysupervised', name=args.dataset, train=False,
-                             num_classes=args.num_classes, data_dir=args.data_dir)
-    eval_dset = _eval_dset.get_dset()
+        _eval_dset = SSL_Dataset(args, alg='fullysupervised', name=args.dataset, train=False,
+                                 num_classes=args.num_classes, data_dir=args.data_dir)
+        eval_dset = _eval_dset.get_dset()
+    else:
+        name, mean, std = args.dataset[7:].split("_", 2)
+        lb_dset = CustomDataset2(args.data_dir, name, True, False, float(mean), float(std))
+        eval_dset = CustomDataset2(args.data_dir, name, False, False, float(mean), float(std))
+
     if args.rank == 0:
         torch.distributed.barrier()
     
