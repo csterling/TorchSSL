3d34899e5b1a2994fbb41ae90e893b3c5b907ad7
csterling <coreytsterling@gmail.com>
2021-12-17 08:58:06 +1300

    Expanded try/catch to also catch import errors.

--- experiments.py
+++ experiments.py
@@ -4,29 +4,29 @@ import traceback
 
 
 def run_experiment(dataset, dataset_size, num_classes, net, ssl_method, pretraining, amount_labelled):
-    sys_main = importlib.import_module(ssl_method).sys_main
-
     save_name = f"{dataset}_{net}_{ssl_method}_{pretraining}_{amount_labelled}"
 
-    epoch_size_in_iterations = dataset_size // 512 + 1
-
-    # Args are supplied args + common settings from config + custom
-    args = list(sys.argv[1:])
-    args += [
-        "--c", f"config/experiments/{ssl_method}.yaml",
-        "--save_name", save_name,
-        "--net", net,
-        "--pretrained", str(pretraining),
-        "--dataset", dataset,
-        "--retain-epochs", "50", "100", "500", "1000",
-        "--num_labels", str(amount_labelled),
-        "--num_train_iter", str(1000 * epoch_size_in_iterations + 1),
-        "--num_eval_iter", str(epoch_size_in_iterations),
-        "--num_classes", str(num_classes)
-    ]
-
     try:
+        sys_main = importlib.import_module(ssl_method).sys_main
+
+        epoch_size_in_iterations = dataset_size // 512 + 1
+
+        # Args are supplied args + common settings from config + custom
+        args = list(sys.argv[1:])
+        args += [
+            "--c", f"config/experiments/{ssl_method}.yaml",
+            "--save_name", save_name,
+            "--net", net,
+            "--pretrained", str(pretraining),
+            "--dataset", dataset,
+            "--retain-epochs", "50", "100", "500", "1000",
+            "--num_labels", str(amount_labelled),
+            "--num_train_iter", str(1000 * epoch_size_in_iterations + 1),
+            "--num_eval_iter", str(epoch_size_in_iterations),
+            "--num_classes", str(num_classes)
+        ]
         sys_main(args)
+
     except Exception:
         with open(f"{save_name}.err", "w") as file:
             file.write(traceback.format_exc())
